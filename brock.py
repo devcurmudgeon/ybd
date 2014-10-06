36b2cfad8bfe156ccb3b13789ec23a41c5706771
Paul Sherwood <paul.sherwood@codethink.co.uk>
2014-10-06 14:18:32 +0100

    tidyup

--- brock.py
+++ brock.py
@@ -2,7 +2,6 @@ import yaml
 
 def load_assembly(foo):
   filename = "./sets/" + foo + ".def"
-  print filename
 
   with open(filename) as f:
     text = f.read()
@@ -10,15 +9,15 @@ def load_assembly(foo):
   assembly = yaml.safe_load(text)
   return assembly
 
-obj = load_assembly("second-set")
+obj = load_assembly("third-set")
 
 for iter in obj['depends']:
   assembly = load_assembly(iter)
-
-  print assembly['depends']
+  print assembly['assembly']
   print '----------------'
+  print assembly['depends']
   for chunk in assembly['chunks']:
     print chunk['lookup']
-    print '****' 
+    print '===='
     print chunk['depends']
 
