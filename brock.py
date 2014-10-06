f54b4f6f8f070ea491e0eaa11671a9fc16877461
Paul Sherwood <paulsherwood@codethink.co.uk>
2014-10-06 22:18:02 +0100

    count number of hits

--- brock.py
+++ brock.py
@@ -24,30 +24,37 @@ def get(thing, value):
 		pass
 	return val
 
+def graph_count(name):
+	try:
+	    graph[name] = graph[name] + 1
+
+	except:
+	    graph[name] = 0
+
 def walk(name, graph):
 	print
 	print
 	print 'Walk %s' % name
 	this = load_assembly(name)
-	graph[name] = '1'
+	graph_count(name)
 
 	for dependency in get(this, 'build-depends'):
 	    print '%s build-dependency is %s' % (get(this, 'name'), dependency)
-	    graph[dependency] = '1'
+	    graph_count(dependency)
 	    walk(dependency, graph)
 
 	for content in get(this, 'contents'):
 		print '%s contains %s, check for build-dependencies:' % (name, get(content, 'name'))
 		name = get(content, 'name').split('|')[0]
-		graph[name] = '1'
+		graph_count(name)
 		print '-- %s' % get(content, 'build-depends')
 		for dep in get(content, 'build-depends'):
-		    graph[name] = '1'
+		    graph_count(dep)
 		if load_assembly(get(content, 'name')):
 		    walk(get(content, 'name'), graph)
 
 graph = {}
-walk('fifth-set', graph)
+walk('first-set', graph)
 
 print graph
 
