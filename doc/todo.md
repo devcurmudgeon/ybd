25ca6ccefb2ae11a0c4e3cdc30d09eb4528f8ca3
Paul Sherwood <paul.sherwood@codethink.co.uk>
2014-10-12 11:29:28 +0100

    Add todo, and morph-converter.sh

---
+++ doc/todo.md
@@ -0,0 +1,9 @@
+TODO
+- converter script for morph definitions to brock format
+- test brock on real definitions
+- logging
+- config
+- arch handling
+- add distbuild queuing
+- implement actual build functionality
+- implement deploy functionality
---
+++ morph-converter.sh
@@ -0,0 +1,5 @@
+find . -name *morph | xargs sed -i -e "s/^- morph: strata\//- name: /"
+find . -name *morph | xargs sed -i -e "s/^- morph: systems\//- name: /"
+find . -name *morph | xargs sed -i -e "s/\.morph//"
+find . -name *morph | xargs sed -i -e "s/strata:/contents:/"
+find . -name *morph | xargs sed -i -e "s/chunks:/contents:/"
