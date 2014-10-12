f56f7ffa077974fbe0c2ebcc5dcdb70ea11e20dd
Paul Sherwood <paul.sherwood@codethink.co.uk>
2014-10-12 08:45:35 +0100

    PEP8 fixes

--- brock.py
+++ brock.py
@@ -3,131 +3,143 @@ import os
 import sys
 import hashlib
 
+
 def load_defs(path, definitions):
-	for dirname, dirnames, filenames in os.walk("."):
-		for filename in filenames:
-			if not filename.endswith('.def'):
-				continue
+    for dirname, dirnames, filenames in os.walk("."):
+        for filename in filenames:
+            if not filename.endswith('.def'):
+                continue
 
-			this = load_def(dirname, filename)
-			name = get(this, 'name')
-			if name != []:
-				for i, definition in enumerate(definitions):
-					if definition['name'] == this['name']:
-						definitions[i] = this
+            this = load_def(dirname, filename)
+            name = get(this, 'name')
+            if name != []:
+                for i, definition in enumerate(definitions):
+                    if definition['name'] == this['name']:
+                        definitions[i] = this
 
-				if get(definitions, 'name') == []:
-					definitions.append(this)
+                if get(definitions, 'name') == []:
+                    definitions.append(this)
 
-				for dependency in get(this, 'build-depends'):
-					# print 'dependency is %s' % dependency
-					if get(dependency, 'repo') != []:
-						dependency['hash'] = this['hash']
-						definitions.append(dependency)
+                for dependency in get(this, 'build-depends'):
+                    # print 'dependency is %s' % dependency
+                    if get(dependency, 'repo') != []:
+                        dependency['hash'] = this['hash']
+                        definitions.append(dependency)
 
-				for content in get(this, 'contents'):
-					# print 'content is %s' % content
-					if get(content, 'repo') != []:
-						content['hash'] = this['hash']
-						definitions.append(content)
+                for content in get(this, 'contents'):
+                    # print 'content is %s' % content
+                    if get(content, 'repo') != []:
+                        content['hash'] = this['hash']
+                        definitions.append(content)
 
-		if '.git' in dirnames:
-			dirnames.remove('.git')
+        if '.git' in dirnames:
+            dirnames.remove('.git')
 
-#	for i in definitions:
-#		print
-#		print i
+#   for i in definitions:
+#       print
+#       print i
+
 
 def load_def(path, name):
-	try:
-		with open(path + "/" + name) as f:
-			text = f.read()
+    try:
+        with open(path + "/" + name) as f:
+            text = f.read()
 
-		definition = yaml.safe_load(text)
-		definition['hash'] = hashlib.sha256(path + "/" + name).hexdigest()[:8]
+        definition = yaml.safe_load(text)
+        definition['hash'] = hashlib.sha256(path + "/" + name).hexdigest()[:8]
 
-	except ValueError:
-		print 'Oops, problem loading %s' % (path + "/" + name)
+    except ValueError:
+        print 'Oops, problem loading %s' % (path + "/" + name)
 
-	return definition
+    return definition
+
 
 def get_definition(definitions, this):
-	if get(this, 'contents') != [] or get(this, 'repo') != [] and get(this, 'ref') != []:
-		return this
+    if (get(this, 'contents') != [] or get(this, 'repo') != [] and
+            get(this, 'ref') != []):
+        return this
 
-	for definition in definitions:
-		if definition['name'] == this:
-			return definition
+    for definition in definitions:
+        if definition['name'] == this:
+            return definition
 
-		if definition['name'].split('|')[0] == this:
-			return definition
+        if definition['name'].split('|')[0] == this:
+            return definition
 
-		if definition['name'] == get(this, 'name'):
-			return definition
+        if definition['name'] == get(this, 'name'):
+            return definition
 
-		if definition['name'].split('|')[0] == get(this, 'name'):
-			return definition
+        if definition['name'].split('|')[0] == get(this, 'name'):
+            return definition
 
-	print "Oh dear, where is the definition of %s, %s?" % (this, get(this, 'name'))
-	raise SystemExit
+    print "Oh dear, where is %s, %s?" % (this, get(this, 'name'))
+    raise SystemExit
+
 
 def get(thing, value):
-	val = []
-	try:
-		val = thing[value]
-	except:
-		pass
-	return val
+    val = []
+    try:
+        val = thing[value]
+    except:
+        pass
+    return val
+
 
 def assemble(definitions, this):
-	print 'assemble %s' % get(this,'name')
+    print 'assemble %s' % get(this, 'name')
+
 
 def touch(pathname):
     with open(pathname, 'w'):
         pass
 
+
 def cache_key(definitions, this):
-	definition = get_definition(definitions, this)
-	# print 'cache_key %s' % definition
-	return path + "/cache/" + definition['name'] + "|" + definition['hash'] + ".cache"
+    definition = get_definition(definitions, this)
+    # print 'cache_key %s' % definition
+    return path + "/cache/" + definition['name'] + "|" + \
+        definition['hash'] + ".cache"
 
 
 def cache(definitions, this):
-	# print 'cache %s' % this
-	touch(cache_key(definitions, this))
+    # print 'cache %s' % this
+    touch(cache_key(definitions, this))
+
 
 def is_cached(definitions, this):
-	if os.path.exists(cache_key(definitions, this)):
-		return True
+    if os.path.exists(cache_key(definitions, this)):
+        return True
 
-	return False
+    return False
 
-	for cache in maybe_caches(this):
-		ref = get_ref(cache)
-		diff = git_diff(DTR, ref)
-		if diff:
-			for dependency in this['build-depends']:
-				return
+    for cache in maybe_caches(this):
+        ref = get_ref(cache)
+        diff = git_diff(DTR, ref)
+        if diff:
+            for dependency in this['build-depends']:
+                return
+
 
 def build(definitions, target):
-	print 'build %s' % target
-	if is_cached(definitions, target):
-		print '%s is cached' % target
-		return
+    print 'build %s' % target
+    if is_cached(definitions, target):
+        print '%s is cached' % target
+        return
 
-	this = get_definition(definitions, target)
+    this = get_definition(definitions, target)
 
-	for dependency in get(this, 'build-depends'):
-		build(definitions, dependency)
+    for dependency in get(this, 'build-depends'):
+        build(definitions, dependency)
 
-	# wait here for all the dependencies to complete 
-	# how do we know when that happens?
+    # wait here for all the dependencies to complete
+    # how do we know when that happens?
 
-	for content in get(this, 'contents'):
-		build(definitions, content)
+    for content in get(this, 'contents'):
+        build(definitions, content)
 
-	assemble(definitions, this)
-	cache(definitions, this)
+    assemble(definitions, this)
+    cache(definitions, this)
+
 
 definitions = []
 path, target = os.path.split(sys.argv[1])
