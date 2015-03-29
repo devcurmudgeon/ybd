devcurmudgeon/ybd

commit 462d7bda7f0f20f7fd885dfd9f03f4d0465303cc

Need to install depends of depends

sandbox.py
@@ -81,11 +81,18 @@ def remove(this):
         shutil.rmtree(this['assembly'])
 
 
-def install(this, component, permit_bootstrap=True):
-    if component.get('build-mode') == 'bootstrap' and permit_bootstrap == False:
-        return
+def install(this, component):
     for subcomponent in component.get('contents', []):
-        install(this, subcomponent, False)
+        install_artifact(this, subcomponent)
+    for dependency in component.get('build-depends', []):
+        install_artifact(this, dependency)
+    install_artifact(this, component)
+
+
+def install_artifact(this, component):
+    component = Definitions().get(component)
+    if component.get('build-mode') == 'bootstrap':
+        return
     app.log(this, 'Installing %s' % component['cache'])
     unpackdir = cache.unpack(component)
     utils.hardlink_all_files(unpackdir, this['assembly'])

commit 608d31198292c17dba916b54d4763604f794fee6

462d7bda7f0 broke the bootstrap install stuff, this fixes that

sandbox.py
@@ -81,19 +81,19 @@ def remove(this):
         shutil.rmtree(this['assembly'])
 
 
-def install(this, component):
+def install(this, component, permit_bootstrap=True):
     for subcomponent in component.get('contents', []):
-        install_artifact(this, subcomponent)
+        install_artifact(this, subcomponent, False)
     for dependency in component.get('build-depends', []):
-        install_artifact(this, dependency)
-    install_artifact(this, component)
+        install_artifact(this, dependency, False)
+    install_artifact(this, component, permit_bootstrap)
 
 
-def install_artifact(this, component):
+def install_artifact(this, component, permit_bootstrap=True):
     component = Definitions().get(component)
-    if component.get('build-mode') == 'bootstrap':
+    if component.get('build-mode') == 'bootstrap' and permit_bootstrap == False:
         return
-#    app.log(this, 'Installing %s' % component['cache'])
+    app.log(this, 'Installing %s' % component['cache'])
     unpackdir = cache.unpack(component)
     utils.hardlink_all_files(unpackdir, this['assembly'])
 