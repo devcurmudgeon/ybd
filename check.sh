3177c44963a6b9529ce14177c51818321c3d6187
Paul Sherwood <paul.sherwood@codethink.co.uk>
2015-05-10 10:33:22 +0000

    Remove check.sh

--- check.sh
+++
@@ -1,9 +0,0 @@
-echo
-echo
-set -v
-rm -fr ~/.ybd/cache/ybd-artifacts/*
-rm -fr ~/.ybd/staging/*
-rm -fr /src/staging/*
-rm -fr /src/tmp/*
-# rm -fr /src/cache/ybd-artifacts/*
-python /src/ybd/ybd.py $1 $2
