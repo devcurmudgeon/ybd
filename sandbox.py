devcurmudgeon/ybd

commit 66cf5ba0b20ec4f6e3c1d99e1b420b455203cef7

Replace sandboxing code with the external 'sandboxlib' library
YBD now uses the linux_user_chroot backend from sandboxlib 0.1.1.
Behaviour should be totally unchanged as it is effectively the same code
at this point.

In future we can try to support multiple sandboxlib backends. This will
probably require improvements to sandboxlib to better specify what
features each backend supports. The benefit will be that
linux-user-chroot should not be required any more, making YBD more
portable.

sandbox.py
@@ -14,18 +14,29 @@
 #
 # =*= License: GPL-2 =*=
 
+
+import sandboxlib
+
 import contextlib
 import os
-import textwrap
-from subprocess import call, PIPE, check_output
-import app
-from definitions import Definitions
+import pipes
 import shutil
-import utils
-import cache
-from repos import get_repo_url
-import tempfile
 import stat
+import tempfile
+from subprocess import call, PIPE
+
+import app
+import cache
+from definitions import Definitions
+from repos import get_repo_url
+import utils
+
+
+def builddir_for_component(this):
+    return this['name'] + '.build'
+
+def installdir_for_component(this):
+    return this['name'] + '.inst'
 
 
 @contextlib.contextmanager
@@ -35,8 +46,10 @@ def setup(this):
 
     tempfile.tempdir = app.settings['staging']
     this['sandbox'] = tempfile.mkdtemp()
-    this['build'] = os.path.join(this['sandbox'], this['name'] + '.build')
-    this['install'] = os.path.join(this['sandbox'], this['name'] + '.inst')
+    this['build'] = os.path.join(
+        this['sandbox'], builddir_for_component(this))
+    this['install'] = os.path.join(
+        this['sandbox'], installdir_for_component(this))
     this['baserockdir'] = os.path.join(this['install'], 'baserock')
     this['tmp'] = os.path.join(this['sandbox'], 'tmp')
     for directory in ['build', 'install', 'tmp', 'baserockdir']:
@@ -142,55 +155,94 @@ def run_sb(this, command, allow_parallel=False):
                     os.environ["MAKEFLAGS"] = cur_makeflags
 
 
+def argv_to_string(argv):
+    return ' '.join(map(pipes.quote, argv))
+
+
 def run_sandboxed(this, command, allow_parallel=False):
     app.log(this, 'Running command:\n%s' % command)
     with open(this['log'], "a") as logfile:
         logfile.write("# # %s\n" % command)
-    rw_root = True if this.get('kind') == 'system' else False
-    use_chroot = False if this.get('build-mode') == 'bootstrap' else True
-    do_not_mount_dirs = [this['build'], this['install']]
 
-    if use_chroot:
-        chroot_dir = this['sandbox']
-        chdir = os.path.join('/', os.path.basename(this['build']))
-        do_not_mount_dirs += [os.path.join(this['sandbox'], d)
-                              for d in ["dev", "proc", 'tmp']]
-        mounts = ('dev/shm', 'tmpfs', 'none'),
+    executor = sandboxlib.linux_user_chroot
+    sandbox_config = executor.maximum_possible_isolation()
+
+    mounts = ccache_mounts(this)
+
+    if this.get('build-mode') == 'bootstrap':
+        # bootstrap mode: builds have some access to the host system, so they
+        # can use the compilers etc.
+        tmpdir = app.settings.get("TMPDIR", "/tmp")
+
+        writable_paths = [
+            this['build'], this['install'], tmpdir,
+        ]
+
+        sandbox_config.update(dict(
+            cwd=this['build'],
+            filesystem_root='/',
+            filesystem_writable_paths=writable_paths,
+            extra_mounts=[],
+        ))
     else:
-        chroot_dir = '/'
-        chdir = this['build']
-        do_not_mount_dirs += [app.settings.get("TMPDIR", "/tmp")]
-        mounts = []
+        # normal mode: builds run in a chroot with only their dependencies
+        # present.
 
-    binds = get_binds(this)
+        mounts.extend([
+            (None, '/dev/shm', 'tmpfs'),
+            (None, '/proc', 'proc'),
+        ])
 
-    container_config = dict(
-        cwd=chdir,
-        root=chroot_dir,
-        mounts=mounts,
-        mount_proc=use_chroot,
-        binds=binds,
-        writable_paths=None if rw_root else do_not_mount_dirs)
+        if this.get('kind') == 'system':
+            writable_paths = None
+        else:
+            writable_paths = [
+                builddir_for_component(this),
+                installdir_for_component(this),
+                '/dev', '/proc', '/tmp',
+            ]
+
+        sandbox_config.update(dict(
+            cwd=builddir_for_component(this),
+            filesystem_root=this['sandbox'],
+            filesystem_writable_paths=writable_paths,
+            extra_mounts=mounts,
+        ))
 
     argv = ['sh', '-c', command]
-    cmd_list = utils.containerised_cmdline(argv, **container_config)
 
     cur_makeflags = os.environ.get("MAKEFLAGS")
+
     try:
         if not allow_parallel:
             os.environ.pop("MAKEFLAGS", None)
-        run_logged(this, cmd_list)
+
+        # The setup() function modifies os.environ directly to match the build
+        # environment. so there isn't any leakage of host environment
+        # variables here.
+        env = os.environ
+        app.log_env(this['log'], argv_to_string(argv))
+
+        with open(this['log'], "a") as logfile:
+            exit_code = executor.run_sandbox_with_redirection(
+                argv, stdout=logfile, stderr=sandboxlib.STDOUT,
+                env=env, **sandbox_config)
+
+        if exit_code != 0:
+            app.log(this, 'ERROR: command failed in directory %s:\n\n' %
+                    os.getcwd(), argv_to_string(argv))
+            app.exit(this, 'ERROR: log file is at', this['log'])
     finally:
         if cur_makeflags is not None:
             os.environ["MAKEFLAGS"] = cur_makeflags
 
 
 def run_logged(this, cmd_list):
-    app.log_env(this['log'], '\n'.join(cmd_list))
+    app.log_env(this['log'], argv_to_string(cmd_list))
     with open(this['log'], "a") as logfile:
         if call(cmd_list, stdin=PIPE, stdout=logfile, stderr=logfile):
             app.log(this, 'ERROR: command failed in directory %s:\n\n' %
-                    os.getcwd(), ' '.join(cmd_list))
+                    os.getcwd(), argv_to_string(cmd_list))
             app.exit(this, 'ERROR: log file is at', this['log'])
 
 
@@ -233,22 +285,20 @@ def run_extension(this, deployment, step, method):
     return
 
 
-def get_binds(this):
-    if app.settings['no-ccache']:
-        binds = ()
-    elif 'repo' in this:
+def ccache_mounts(this):
+    if app.settings['no-ccache'] or 'repo' not in this:
+        mounts = []
+    else:
         name = os.path.basename(get_repo_url(this['repo']))
+
         ccache_dir = os.path.join(app.settings['ccache_dir'], name)
-        ccache_target = os.path.join(this['sandbox'],
-                                     os.environ['CCACHE_DIR'].lstrip('/'))
         if not os.path.isdir(ccache_dir):
             os.mkdir(ccache_dir)
-        if not os.path.isdir(ccache_target):
-            os.mkdir(ccache_target)
-        binds = ((ccache_dir, ccache_target),)
-    else:
-        binds = ()
-    return binds
+
+        ccache_target = os.environ['CCACHE_DIR']
+
+        mounts = [(ccache_dir, ccache_target, None, 'bind')]
+    return mounts
 
 
 def clean_env(this):
@@ -319,7 +369,7 @@ def create_devices(this):
         mode = int(device['permissions'], 8) & perms_mask
         if device['type'] == 'c':
             mode = mode | stat.S_IFCHR
-        elif dev['type'] == 'b':
+        elif device['type'] == 'b':
             mode = mode | stat.S_IFBLK
         else:
             raise IOError('Cannot create device node %s,'

utils.py
@@ -16,199 +16,11 @@
 
 import glob
 import os
-import textwrap
 import stat
-import time
 import shutil
-import tempfile
-from subprocess import call, check_output
+import textwrap
+
 import app
-
-
-def containerised_cmdline(args, cwd='.', root='/', binds=(),
-                          mount_proc=False, unshare_net=False,
-                          writable_paths=None, **kwargs):
-    '''
-    Describe how to run 'args' inside a linux-user-chroot container.
-
-    The subprocess will only be permitted to write to the paths we
-    specifically allow it to write to, listed in 'writeable paths'. All
-    other locations in the file system will be read-only.
-
-    The 'binds' parameter allows mounting of arbitrary file-systems,
-    such as tmpfs, before running commands, by setting it to a list of
-    (mount_point, mount_type, source) triples.
-
-    The 'root' parameter allows running the command in a chroot, allowing
-    the host file system to be hidden completely except for the paths
-    below 'root'.
-
-    The 'mount_proc' flag enables mounting of /proc inside 'root'.
-    Locations from the file system can be bind-mounted inside 'root' by
-    setting 'binds' to a list of (src, dest) pairs. The 'dest'
-    directory must be inside 'root'.
-
-    The subprocess will be run in a separate mount namespace. It can
-    optionally be run in a separate network namespace too by setting
-    'unshare_net'.
-
-    '''
-
-    if not root.endswith('/'):
-        root += '/'
-    if writable_paths is None:
-        writable_paths = (root,)
-
-    cmdargs = ['linux-user-chroot', '--chdir', cwd]
-    if unshare_net:
-        cmdargs.append('--unshare-net')
-    for src, dst in binds:
-        # linux-user-chroot's mount target paths are relative to the chroot
-        cmdargs.extend(('--mount-bind', src, os.path.relpath(dst, root)))
-    for d in invert_paths(os.walk(root), writable_paths):
-        if not os.path.islink(d):
-            cmdargs.extend(('--mount-readonly', os.path.relpath(d, root)))
-    if mount_proc:
-        proc_target = os.path.join(root, 'proc')
-        if not os.path.exists(proc_target):
-            os.makedirs(proc_target)
-        cmdargs.extend(('--mount-proc', 'proc'))
-    cmdargs.append(root)
-    cmdargs.extend(args)
-
-    return unshared_cmdline(cmdargs, root=root, **kwargs)
-
-
-def unshared_cmdline(args, root='/', mounts=()):
-    '''Describe how to run 'args' inside a separate mount namespace.
-
-    This function wraps 'args' in a rather long commandline that ensures
-    the subprocess cannot see any of the system's mounts other than those
-    listed in 'mounts', and mounts done by that command can only be seen
-    by that subprocess and its children. When the subprocess exits all
-    of its mounts will be unmounted.
-
-    '''
-    # We need to do mounts in a different namespace. Unfortunately
-    # this means we have to in-line the mount commands in the
-    # command-line.
-
-    command = textwrap.dedent(r'''
-    mount --make-rprivate /
-    root="$1"
-    shift
-    ''')
-    cmdargs = [root]
-
-    # We need to mount all the specified mounts in the namespace,
-    # we don't need to unmount them before exiting, as they'll be
-    # unmounted when the namespace is no longer used.
-    command += textwrap.dedent(r'''
-    while true; do
-        case "$1" in
-        --)
-            shift
-            break
-            ;;
-        *)
-            mount_point="$1"
-            mount_type="$2"
-            mount_source="$3"
-            shift 3
-            path="$root/$mount_point"
-            mount -t "$mount_type" "$mount_source" "$path"
-            ;;
-        esac
-    done
-    ''')
-    for mount_point, mount_type, source in mounts:
-        path = os.path.join(root, mount_point)
-        if not os.path.exists(path):
-            os.makedirs(path)
-        cmdargs.extend((mount_point, mount_type, source))
-    cmdargs.append('--')
-
-    command += textwrap.dedent(r'''
-    exec "$@"
-    ''')
-    cmdargs.extend(args)
-
-    # The single - is just a shell convention to fill $0 when using -c,
-    # since ordinarily $0 contains the program name.
-    cmdline = ['unshare', '--mount', '--', 'sh', '-ec', command, '-']
-    cmdline.extend(cmdargs)
-    return cmdline
-
-
-def invert_paths(tree_walker, paths):
-    '''List paths from `tree_walker` that are not in `paths`.
-
-    Given a traversal of a tree and a set of paths separated by os.sep,
-    return the files and directories that are not part of the set of
-    paths, culling directories that do not need to be recursed into,
-    if the traversal supports this.
-
-    `tree_walker` is expected to follow similar behaviour to `os.walk()`.
-
-    This function will remove directores from the ones listed, to avoid
-    traversing into these subdirectories, if it doesn't need to.
-
-    As such, if a directory is returned, it is implied that its contents
-    are also not in the set of paths.
-
-    If the tree walker does not support culling the traversal this way,
-    such as `os.walk(root, topdown=False)`, then the contents will also
-    be returned.
-
-    The purpose for this is to list the directories that can be made
-    read-only, such that it would leave everything in paths writable.
-
-    Each path in `paths` is expected to begin with the same path as
-    yielded by the tree walker.
-
-    '''
-
-    def normpath(path):
-        if path == '.':
-            return path
-        path = os.path.normpath(path)
-        if not os.path.isabs(path):
-            path = os.path.join('.', path)
-        return path
-
-    def any_paths_are_subpath_of(prefix):
-        prefix = normpath(prefix)
-        norm_paths = (normpath(path) for path in paths)
-        return any(path[:len(prefix)] == prefix
-                   for path in norm_paths)
-
-    def path_is_listed(path):
-        return any(normpath(path) == normpath(other)
-                   for other in paths)
-
-    for dirpath, dirnames, filenames in tree_walker:
-
-        if path_is_listed(dirpath):
-            # No subpaths need to be considered
-            del dirnames[:]
-            del filenames[:]
-        elif any_paths_are_subpath_of(dirpath):
-            # Subpaths may be marked, or may not, need to leave this
-            # writable, so don't yield, but we don't cull.
-            pass
-        else:
-            # not listed as a parent or an exact match, needs to be
-            # yielded, but we don't need to consider subdirs, so can cull
-            yield dirpath
-            del dirnames[:]
-            del filenames[:]
-
-        for filename in filenames:
-            fullpath = os.path.join(dirpath, filename)
-            if path_is_listed(fullpath):
-                pass
-            else:
-                yield fullpath
 
 
 def copy_all_files(srcpath, destpath):

commit c53c2700b870c14b65cf0263ee629fa956793e47

Fix system builds
This was broken in commit 66cf5ba0b20ec4f6e3c1d99e1b420b455203cef7, sorry.

sandbox.py
@@ -194,7 +194,7 @@ def run_sandboxed(this, command, allow_parallel=False):
         ])
 
         if this.get('kind') == 'system':
-            writable_paths = None
+            writable_paths = 'all'
         else:
             writable_paths = [
                 builddir_for_component(this),