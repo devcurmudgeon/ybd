09d16a73189afc735ee3356f7a7258503a58f657
Paul Sherwood <paul.sherwood@codethink.co.uk>
2015-06-27 21:00:27 +0000

    Lose a couple of lines

--- ybd.py
+++ ybd.py
@@ -31,13 +31,11 @@ import sandbox
 
 
 print('')
-
 app.setup(sys.argv)
 with app.timer('TOTAL', '%s starts, version %s' % (app.settings['program'],
                app.settings['program-version'])):
-    app.log('TARGET', 'Target is %s' % os.path.join(app.settings['defdir'],
-                                                    app.settings['target']),
-                                                    app.settings['arch'])
+    target = os.path.join(app.settings['defdir'], app.settings['target'])
+    app.log('TARGET', 'Target is %s' % target, app.settings['arch'])
     with app.timer('DEFINITIONS', 'Parsing %s' % app.settings['def-version']):
         defs = Definitions()
     with app.timer('CACHE-KEYS', 'Calculating'):
