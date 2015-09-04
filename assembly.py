devcurmudgeon/ybd

commit 4dad76da37780ff3e5fcd9e41f2f1e74e8c8e1ec

Make assembly robust against discovery of out-of-order cache items V2

assembly.py
@@ -49,23 +49,19 @@ def assemble(defs, target):
         random.shuffle(systems)
         for system in systems:
             assemble(defs, system['path'])
-
             for subsystem in system.get('subsystems', []):
                 assemble(defs, subsystem)
-
 
         dependencies = component.get('build-depends', [])
         random.shuffle(dependencies)
         for it in dependencies:
-            dependency = defs.get(it)
-            assemble(defs, dependency)
-            sandbox.install(defs, component, dependency)
+            preinstall(defs, component, it)
 
         contents = component.get('contents', [])
         random.shuffle(contents)
         for it in contents:
             subcomponent = defs.get(it)
-            if subcomponent.get('build-mode') != 'bootstrap':
+            if subcomponent.get('build-mode', 'staging') != 'bootstrap':
                 assemble(defs, subcomponent)
                 sandbox.install(defs, component, subcomponent)
 
@@ -80,6 +76,32 @@ def assemble(defs, target):
         sandbox.remove(component)
 
     return cache.cache_key(defs, component)
+
+
+def preinstall(defs, component, it):
+    '''Install it and all its recursed dependencies into component sandbox.'''
+    dependency = defs.get(it)
+    if os.path.exists(os.path.join(component['sandbox'], 'baserock',
+                                   dependency['name'] + '.meta')):
+        return
+
+    dependencies = dependency.get('build-depends', [])
+    random.shuffle(dependencies)
+    for dep in dependencies:
+        it = defs.get(dep)
+        if (it.get('build-mode', 'staging') ==
+                dependency.get('build-mode', 'staging')):
+         preinstall(defs, component, it)
+
+    contents = dependency.get('contents', [])
+    random.shuffle(contents)
+    for sub in contents:
+        it = defs.get(sub)
+        if it.get('build-mode', 'staging') != 'bootstrap':
+            preinstall(defs, component, it)
+
+    assemble(defs, dependency)
+    sandbox.install(defs, component, dependency)
 
 
 def build(defs, this):

sandbox.py
@@ -81,26 +81,6 @@ def install(defs, this, component):
                                    component['name'] + '.meta')):
         return
 
-    _install(defs, this, component)
-
-
-def _install(defs, this, component):
-    if os.path.exists(os.path.join(this['sandbox'], 'baserock',
-                                   component['name'] + '.meta')):
-        return
-
-    app.log(this, 'Installing %s' % component['cache'])
-    for it in component.get('build-depends', []):
-        dependency = defs.get(it)
-        if (dependency.get('build-mode', 'staging') ==
-                component.get('build-mode', 'staging')):
-            _install(defs, this, dependency)
-
-    for it in component.get('contents', []):
-        subcomponent = defs.get(it)
-        if subcomponent.get('build-mode', 'staging') != 'bootstrap':
-            _install(defs, this, subcomponent)
-
     unpackdir = cache.get_cache(defs, component) + '.unpacked'
     if this.get('kind') is 'system':
         utils.copy_all_files(unpackdir, this['sandbox'])

commit d87eca37730a1d56b5b8aa407dde6f15cae528c0

4dad76d broke assembly of systems

assembly.py
@@ -63,8 +63,7 @@ def assemble(defs, target):
         for it in contents:
             subcomponent = defs.get(it)
             if subcomponent.get('build-mode', 'staging') != 'bootstrap':
-                assemble(defs, subcomponent)
-                sandbox.install(defs, component, subcomponent)
+                preinstall(defs, component, subcomponent)
 
         app.config['counter'] += 1
         if 'systems' not in component: