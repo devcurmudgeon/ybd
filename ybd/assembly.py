devcurmudgeon/ybd

commit be4cedb76f4406ed2da1ad67864d46b4d748068f

Only install dependencies for chunks, should fix #150

ybd/assembly.py
@@ -100,15 +100,15 @@ def build(defs, component):
     if get_cache(defs, component):
         return
 
-    if 'systems' not in component:
-        with claim(defs, component):
+    with claim(defs, component):
+        if component.get('kind', 'chunk') == 'chunk':
             install_dependencies(defs, component)
-            with app.timer(component, 'build of %s' % component['cache']):
-                run_build(defs, component)
+        with app.timer(component, 'build of %s' % component['cache']):
+            run_build(defs, component)
 
-            with app.timer(component, 'artifact creation'):
-                do_manifest(component)
-                cache(defs, component)
+        with app.timer(component, 'artifact creation'):
+            do_manifest(component)
+            cache(defs, component)
 
 
 def run_build(defs, this):

commit 91554a991659b365b5517529813f7e3468a37e32

Fix multi-instance logic, was broken by be4cedb

ybd/assembly.py
@@ -48,7 +48,6 @@ class RetryException(Exception):
         for dirname in app.config['sandboxes']:
             app.remove_dir(dirname)
         app.config['sandboxes'] = []
-        pass
 
 
 def compose(defs, target):
@@ -172,10 +171,9 @@ def claim(defs, this):
         try:
             with open(lockfile(defs, this), 'a') as l:
                 fcntl.flock(l, fcntl.LOCK_EX | fcntl.LOCK_NB)
-                try:
-                    yield
-                finally:
-                    return
+                yield
+            os.remove(lockfile(defs, this))
+            return
         except IOError as e:
             raise RetryException(defs, this)
     else: