5454415320c93c661f4e7f37ccd6834d2e3e660d
Paul Sherwood <paul.sherwood@codethink.co.uk>
2016-02-15 14:52:59 +0000

    Revert "Call update_mirror() from get_tree() and mirror()"
    This reverts commit b902dae48e0380409c89505e95b69f0a8932908c.
    
    The simplification leads to some breakage, for example on binutils-gdb
    
    root@vagrant-ubuntu-trusty-64:~/ybd/tmp/tmpx14QBG# git fetch git://git.baserock.org/delta/binutils-gdb +refs/*:refs/*
    remote: Counting objects: 1995, done.
    remote: Compressing objects: 100% (1165/1165), done.
    remote: Total 1995 (delta 1743), reused 993 (delta 825)
    Receiving objects: 100% (1995/1995), 1.13 MiB | 1.43 MiB/s, done.
    Resolving deltas: 100% (1743/1743), completed with 688 local objects.
    From git://git.baserock.org/delta/binutils-gdb
     * [new ref]         refs/gitano/admin -> refs/gitano/admin
     * [new branch]      palves/cxx-conversion-attempt -> palves/cxx-conversion-attempt
     * [new branch]      users/hjl/bfd_plugin_object -> users/hjl/bfd_plugin_object
     * [new branch]      users/hjl/gcore -> users/hjl/gcore
     * [new branch]      users/hjl/help -> users/hjl/help
    error: 'refs/heads/users/hjl/lto-mixed/master' exists; cannot create 'refs/heads/users/hjl/lto-mixed'
     ! [new branch]      users/hjl/lto-mixed -> users/hjl/lto-mixed  (unable to update local ref)
     * [new branch]      users/hjl/master -> users/hjl/master
     * [new branch]      users/hjl/no-plt -> users/hjl/no-plt
     * [new branch]      users/hjl/plugin-mixed -> users/hjl/plugin-mixed
     * [new branch]      users/hjl/pr17898 -> users/hjl/pr17898
     * [new branch]      users/hjl/pr17975 -> users/hjl/pr17975
     * [new branch]      users/hjl/pr18176 -> users/hjl/pr18176
     * [new branch]      users/hjl/pr18209 -> users/hjl/pr18209
     * [new branch]      users/hjl/pr18289 -> users/hjl/pr18289
     * [new branch]      users/hjl/pr18451 -> users/hjl/pr18451
     * [new branch]      users/hjl/pr19615/binutils-2_26-branch -> users/hjl/pr19615/binutils-2_26-branch
     * [new branch]      users/hjl/releax -> users/hjl/releax
     * [new branch]      users/hjl/zlib -> users/hjl/zlib
     * [new branch]      users/mmetzger/btrace-non-stop -> users/mmetzger/btrace-non-stop
     * [new branch]      users/mmetzger/disasm-vec -> users/mmetzger/disasm-vec
     * [new branch]      users/mwk/record-s390 -> users/mwk/record-s390
     * [new branch]      users/mwk/trace-test-cleanup -> users/mwk/trace-test-cleanup
     * [new branch]      users/palves/all-stop-non-stop -> users/palves/all-stop-non-stop
     * [new branch]      users/palves/checkpoint-ns-stale-gdbflags -> users/palves/checkpoint-ns-stale-gdbflags
     * [new branch]      users/palves/disp-step-no-symbols -> users/palves/disp-step-no-symbols
     * [new branch]      users/palves/gdbserver-crash-without-multiprocess -> users/palves/gdbserver-crash-without-multiprocess
     * [new branch]      users/palves/gdbserver-fork-issues -> users/palves/gdbserver-fork-issues
     * [new branch]      users/palves/gnulib-strtok_r -> users/palves/gnulib-strtok_r
     * [new branch]      users/palves/gnulib-update -> users/palves/gnulib-update
     * [new branch]      users/palves/inferior_spawn_id -> users/palves/inferior_spawn_id
     * [new branch]      users/palves/mingw-attribute-gnu_printf -> users/palves/mingw-attribute-gnu_printf
     * [new branch]      users/palves/mingw-cxx-conversion -> users/palves/mingw-cxx-conversion
     * [new branch]      users/palves/show-which-thread-caused-stop -> users/palves/show-which-thread-caused-stop
     * [new branch]      users/palves/thread-ids-per-inferior -> users/palves/thread-ids-per-inferior
     * [new branch]      users/roland/osabi-assert -> users/roland/osabi-assert
     * [new tag]         gdb-7.9-release -> gdb-7.9-release
    error: some local refs could not be updated; try running
     'git remote prune git://git.baserock.org/delta/binutils-gdb' to remove any old, conflicting branches

--- ybd/repos.py
+++ ybd/repos.py
@@ -100,7 +100,7 @@ def get_tree(this):
                 stderr=fnull):
             # can't resolve this ref. is it upstream?
             app.log(this, 'Fetching from upstream to resolve %s' % ref)
-            update_mirror(this['name'], this['repo'], gitdir)
+            call(['git', 'fetch', 'origin'], stdout=fnull, stderr=fnull)
 
         try:
             tree = check_output(['git', 'rev-parse', ref + '^{tree}'],
@@ -125,7 +125,12 @@ def mirror(name, repo):
             call(['wget', os.path.join(app.config['tar-url'], tar_file)])
             call(['tar', 'xf', tar_file], stderr=fnull)
             os.remove(tar_file)
-            update_mirror(name, repo, tmpdir)
+            call(['git', 'config', 'remote.origin.url', repo_url])
+            call(['git', 'config', 'remote.origin.mirror', 'true'])
+            if call(['git', 'config', 'remote.origin.fetch',
+                     '+refs/*:refs/*']) != 0:
+                raise BaseException('Did not get a valid git repo')
+            call(['git', 'fetch', 'origin'])
     except:
         app.log(name, 'Try git clone from', repo_url)
         with open(os.devnull, "w") as fnull:
