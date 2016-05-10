devcurmudgeon/ybd

commit 779d3e70a288da0956e8c9459e00f6d6edde5c4c

Force new cache-key for split systems

ybd/cache.py
@@ -107,6 +107,9 @@ def hash_factors(defs, definition):
                 defs.defaults.build_systems.get(definition.get('build-system',
                                                                'manual'))
 
+    if app.config.get('default-splits') and definition.get('kind') == 'system':
+        hash_factors['default-splits'] = app.config['default-splits']
+
     return hash_factors
 
 

commit 3155ba963476042bc82c6feb94ab26f897138711

Only support split systems from artifact-version 3
Commit 779d3e70 broke the CI check of historic cache-keys

ybd/cache.py
@@ -106,9 +106,9 @@ def hash_factors(defs, definition):
             hash_factors['default-build-systems'] = \
                 defs.defaults.build_systems.get(definition.get('build-system',
                                                                'manual'))
-
-    if app.config.get('default-splits') and definition.get('kind') == 'system':
-        hash_factors['default-splits'] = app.config['default-splits']
+            if (app.config.get('default-splits') and
+                    definition.get('kind') == 'system'):
+                hash_factors['default-splits'] = app.config['default-splits']
 
     return hash_factors
 