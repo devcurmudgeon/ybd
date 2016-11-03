9a7552b487fed9b54f402998034194251d8c526f
Javier Jardón <jjardon@gnome.org>
2016-11-03 12:14:42 +0000

    install_dependencies.sh: Use 'python3 get-pip.py'

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -96,8 +96,7 @@ fi
 pip3 --version 2>&1 > /dev/null
 if [ $? -ne 0 ]; then
     wget https://bootstrap.pypa.io/get-pip.py
-    chmod +x get-pip.py
-    $SUDO ./get-pip.py
+    $SUDO python3 get-pip.py
     $SUDO rm get-pip.py
 fi
 
